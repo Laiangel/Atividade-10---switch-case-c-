1172f0bd12b5a2cccdcbfdd8f0c03658b4ace684
Laiana angel <laianaangel57@gmail.com>
2024-11-25 10:16:16 -0300

    Add files via upload

---
+++ atividade10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace atividade10
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Digite um número para calcular o fatorial:");
+            if (!int.TryParse(Console.ReadLine(), out int numero) || numero < 0)
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro não negativo.");
+                return;
+            }
+
+           
+            switch (numero)
+            {
+                case 0:
+                case 1:
+                    Console.WriteLine($"O fatorial de {numero} é 1.");
+                    break;
+
+                default:
+                   
+                    long fatorial = 1;
+                    for (int i = 2; i <= numero; i++)
+                    {
+                        fatorial *= i;
+                    }
+                    Console.WriteLine($"O fatorial de {numero} é {fatorial}.");
+                    break;
+            }
+        }
+    }
+}
+
